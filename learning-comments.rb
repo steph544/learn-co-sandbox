bbc55ad15731254d13699b240a2d6d7953606b6c
Stephanie Cheney <steph@scheneyphotography.com>
2020-08-22 03:48:05 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,18 @@
+#run_code_inside = false
+#puts "Code before if...end"
+#if run_code_inside
+ # puts "code inside"
+#end
+#puts "code after if... end"
+
+chance_of_rain = 1000
+if chance_of_rain <= 0.25
+  puts "Pack a sun shelter"
+elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75) 
+  puts "Pack an umbrella"
+else
+  puts "Stay home and read Hegel."
+end
+
+this_year = time.now.year 
+puts "Hey, its not 2019" unless this_year == 2019
